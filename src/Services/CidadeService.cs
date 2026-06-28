82a6379799c176101bc522b23c7922f59eddf3eb
Arthur <arthuralvesgab@gmail.com>
2026-06-28 12:48:02 -0300

    Add comments for clarity in CidadeService methods

--- src/Services/CidadeService.cs
+++ src/Services/CidadeService.cs
@@ -2,26 +2,27 @@ using System.Text.Json;
 
 public class CidadeService
 {
-    private readonly HttpClient _http;
+    private readonly HttpClient _http; //Faz o resquisito externo 
     public CidadeService(HttpClient http)
     {
         _http = http;
     }
+
     public async Task<(string? nome, string? uf, double lat, double lon)> BuscarCidade(string nome) 
     {
-        var url = $"https://brasilapi.com.br/api/cptec/v1/cidade/{nome}";
-        var response = await _http.GetAsync(url);
+        var url = $"https://brasilapi.com.br/api/cptec/v1/cidade/{nome}"; // Api retorna o nome e informações da cidade, caso não encontre retorna 404
+        var response = await _http.GetAsync(url); // Faz a requisição para a Api
 
-        if (!response.IsSuccessStatusCode)
+        if (!response.IsSuccessStatusCode) // Tratamento de erro HTTP, caso a Api de erro retorna o nome invalido e api fora do ar
         return (null, null, 0, 0);
 
-        var json = await response.Content.ReadAsStringAsync();
+        var json = await response.Content.ReadAsStringAsync(); // converte o json
         var cidades = JsonSerializer.Deserialize<List<dynamic>>(json);
 
-        if (cidades == null || cidades.Count == 0)
+        if (cidades == null || cidades.Count == 0) //validação da resposta
         return (null, null, 0, 0);
 
-        var cidade = cidades[0];
+        var cidade = cidades[0]; //pega a primeira cidade da lista
 
         return (cidade.nome,
          cidade.estado,
